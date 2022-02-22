2555c1f69c7ada934be263b337e04d0c15d90e35
Khalid Agha <khalid_agha20@hotmail.com>
2022-02-22 06:41:17 +0000

    updated data structuring code

--- volcano_app/charts_data.py
+++ volcano_app/charts_data.py
@@ -1,9 +1,41 @@
+from pathlib import Path
 import pandas as pd
-import plotly.express as px
 
 
-class Create:
-    def __init__(self, data):
-        self.data = data
+class VolcanoData:
+    def __init__(self):
+        self.volcano = pd.DataFrame()
+        self.eruptions = pd.DataFrame()
+        self.frame = pd.DataFrame()
+        self.import_data()
+        self.countries = []
 
-    def 
+    def import_data(self):
+        volcano_csv = Path(__file__).parent.joinpath('data', 'volcano_list.csv')
+        eruptions_csv = Path(__file__).parent.joinpath('data', 'eruptions_list.csv')
+        self.volcano = pd.read_csv(volcano_csv)
+        self.eruptions = pd.read_csv(eruptions_csv)
+
+    def process_data_for_choropleth(self):
+        self.countries = self.volcano['COUNTRY'].unique().tolist()
+        self.countries.sort()
+        volcano_numbers = self.volcano['COUNTRY'].value_counts()[self.countries]
+        self.frame = pd.DataFrame(list(zip(self.countries, volcano_numbers)), columns= ["COUNTRY", "VOLCANO"])
+
+        eruption_countries = self.eruptions['Country'].unique().tolist()
+        eruption_numbers = self.eruptions['Country'].value_counts()[eruption_countries]
+        eruptions = pd.DataFrame(list(zip(eruption_countries, eruption_numbers)), columns= ["Country", "ERUPTIONS"])
+
+        self.frame = self.frame.merge(eruptions, how="left", left_on="COUNTRY", right_on="Country")
+        self.frame.drop(["Country"], axis = 1, inplace = True)
+        self.frame['ERUPTIONS'] = self.frame['ERUPTIONS'].fillna(0)
+
+
+
+
+
+
+
+x = VolcanoData()
+x.process_data_for_choropleth()
+x.show_data()
