e9ff269c9a2292f8ca3cfcf787a37447d7cc30ef
Khalid Agha <khalid_agha20@hotmail.com>
2022-02-22 19:15:41 +0000

    added code to structure data for line graphs

--- volcano_app/charts_data.py
+++ volcano_app/charts_data.py
@@ -6,36 +6,41 @@ class VolcanoData:
     def __init__(self):
         self.volcano = pd.DataFrame()
         self.eruptions = pd.DataFrame()
-        self.frame = pd.DataFrame()
-        self.import_data()
+        self.iso = pd.DataFrame()
+        self.df_choropleth = pd.DataFrame()
+        self.df_year_eruptions = pd.DataFrame()
         self.countries = []
+        self.import_data()
+
 
     def import_data(self):
         volcano_csv = Path(__file__).parent.joinpath('data', 'volcano_list.csv')
         eruptions_csv = Path(__file__).parent.joinpath('data', 'eruptions_list.csv')
+        iso_csv = Path(__file__).parent.joinpath('data', 'iso.csv')
         self.volcano = pd.read_csv(volcano_csv)
         self.eruptions = pd.read_csv(eruptions_csv)
+        self.iso = pd.read_csv(iso_csv)
+        self.countries = self.eruptions['Country'].unique().tolist()
+        self.countries.sort()
 
     def process_data_for_choropleth(self):
-        self.countries = self.volcano['COUNTRY'].unique().tolist()
-        self.countries.sort()
-        volcano_numbers = self.volcano['COUNTRY'].value_counts()[self.countries]
-        self.frame = pd.DataFrame(list(zip(self.countries, volcano_numbers)), columns= ["COUNTRY", "VOLCANO"])
+        countries = self.volcano['COUNTRY'].unique().tolist()
+        countries.sort()
+        volcano_numbers = self.volcano['COUNTRY'].value_counts()[countries]
+        self.df_choropleth = pd.DataFrame(list(zip(countries, volcano_numbers)), columns= ["COUNTRY", "VOLCANO"])
 
-        eruption_countries = self.eruptions['Country'].unique().tolist()
-        eruption_numbers = self.eruptions['Country'].value_counts()[eruption_countries]
-        eruptions = pd.DataFrame(list(zip(eruption_countries, eruption_numbers)), columns= ["Country", "ERUPTIONS"])
+        eruption_numbers = self.eruptions['Country'].value_counts()[self.countries]
+        eruptions = pd.DataFrame(list(zip(self.countries, eruption_numbers)), columns= ["Country", "ERUPTIONS"])
 
-        self.frame = self.frame.merge(eruptions, how="left", left_on="COUNTRY", right_on="Country")
-        self.frame.drop(["Country"], axis = 1, inplace = True)
-        self.frame['ERUPTIONS'] = self.frame['ERUPTIONS'].fillna(0)
+        self.df_choropleth = self.df_choropleth.merge(eruptions, how="left", left_on="COUNTRY", right_on="Country")
+        self.df_choropleth.drop(["Country"], axis = 1, inplace = True)
+        self.df_choropleth['ERUPTIONS'] = self.df_choropleth['ERUPTIONS'].fillna(0)
+        self.df_choropleth["RATIO"]= self.df_choropleth["ERUPTIONS"]/self.df_choropleth["VOLCANO"]
+        self.df_choropleth=self.df_choropleth.merge(self.iso, how="left", left_on="COUNTRY", right_on="Country")
+        self.df_choropleth.drop(["Country"], axis=1, inplace=True)
 
-
-
-
-
-
-
-x = VolcanoData()
-x.process_data_for_choropleth()
-x.show_data()
+    def process_data_for_line_charts(self):
+        years = self.eruptions["Start Year"].unique().tolist()
+        years.sort()
+        eruptions_per_year = self.eruptions["Start Year"].value_counts()[years]
+        self.df_year_eruptions = pd.DataFrame(list(zip(years, eruptions_per_year)), columns=["YEAR", "ERUPTIONS"])
