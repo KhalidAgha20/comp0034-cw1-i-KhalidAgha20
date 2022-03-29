6e4c34a6138da3d68e24bd9cf2eeaffc568b9c5f
Khalid Agha <khalid_agha20@hotmail.com>
2022-03-29 17:00:55 +0100

    separated cw1 and cw2 dash

---
+++ cw1_dash/apps/app3/app3.py
@@ -0,0 +1,55 @@
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from dash import dcc, html, Input, Output
+
+from cw1_dash.app import app
+from cw1_dash.charts_data import VolcanoData
+import cw1_dash.create_charts as CC
+
+data = VolcanoData()
+data.process_data_for_choropleth()
+cc = CC.MapBox(data)
+fig1 = cc.create_mapbox("Algeria")
+
+layout = dbc.Container(fluid=True, children=[
+    dbc.Row([
+        dbc.Col(width=1),
+
+        dbc.Col(children=[
+            html.Br(),
+
+            html.H1(children='Data for Individual Countries'),
+
+            html.P(
+                children="Geographical locations of volcanoes and their number of eruptions is computed for "
+                         "individual countries. Please note that, in this Web App , a country and its overseas "
+                         "territories are considered as one.")
+        ]),
+
+        dbc.Col(width=1)
+    ]),
+
+    html.Br(),
+    dbc.Row([
+        dbc.Col(width=1),
+        dbc.Col(width=3, children=[
+            html.H6("Select Country"),
+            dcc.Dropdown(data.v_countries,
+                         id="country-dropdown",
+                         value="Algeria")
+        ])
+    ]),
+    dcc.Graph(id="mapbox",
+              figure=fig1)
+])
+
+
+@app.callback(
+    Output('mapbox', 'figure'),
+    Input('country-dropdown', 'value')
+)
+def update_mapbox(value):
+    fig1 = cc.create_mapbox(value)
+    return fig1
