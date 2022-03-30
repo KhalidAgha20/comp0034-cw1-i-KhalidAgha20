c40ff4903ca13ef6ea8b6045e6812939a7269fc6
Khalid Agha <khalid_agha20@hotmail.com>
2022-03-30 01:09:41 +0100

    fixed dash app

---
+++ mydash/apps/app1/app1.py
@@ -0,0 +1,84 @@
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from dash import dcc, html, Input, Output
+
+from mydash.app import app
+from mydash.charts_data import VolcanoData
+import mydash.create_charts as CC
+
+data = VolcanoData()
+data.process_data_for_choropleth()
+cc = CC.Choropleth(data)
+fig1 = cc.create_choropleth("VOLCANO")
+fig2 = cc.create_choropleth("ERUPTIONS")
+fig3 = cc.create_choropleth("RATIO")
+
+bc = CC.BarGraph(data)
+fig4 = bc.creat_bar_graph("VOLCANO")
+fig5 = bc.creat_bar_graph("ERUPTIONS")
+fig6 = bc.creat_bar_graph("RATIO")
+
+layout = dbc.Container(fluid=True, children=[
+    dbc.Row([
+        dbc.Col(width=1),
+
+        dbc.Col(children=[
+            html.Br(),
+
+            html.H1(children='Volcanoes on the World Map'),
+
+            html.P(
+                children="The figures for the number of volcanoes and eruptions have been plotted on a world map to "
+                         "visualize the density of volcanoes around the world. The top 10 countries from each "
+                         "category are compared using a bar chart."),
+        ]),
+
+        dbc.Col(width=1)
+    ]),
+
+    html.Br(),
+
+    dbc.Row(children=[
+        dbc.Col(width=1),
+
+        dbc.Col(width=10, children=[
+            dbc.Tabs(children=[
+                dbc.Tab(dbc.Row([
+                    dbc.Col(width=4, children=[
+                        dcc.Graph(id="volcano-top10", figure=fig4)
+                    ]),
+                    dbc.Col(width=8, children=[
+                        dcc.Graph(id="volcano-choropleth", figure=fig1)
+                    ])
+                ]),
+                    label="Number Of Volcanoes"
+                ),
+
+                dbc.Tab(dbc.Row([
+                    dbc.Col(width=4, children=[
+                        dcc.Graph(id="eruption-top10", figure=fig5)
+                    ]),
+                    dbc.Col(width=8, children=[
+                        dcc.Graph(id="eruption-choropleth", figure=fig2)
+                    ])
+                ]),
+                    label="Number Of Eruptions", ),
+
+                dbc.Tab(dbc.Row([
+                    dbc.Col(width=4, children=[
+                        dcc.Graph(id="ratio-top10", figure=fig6)
+                    ]),
+                    dbc.Col(width=8, children=[
+                        dcc.Graph(id="ratio-choropleth", figure=fig3)
+                    ])
+                ]),
+                    label="Ratio of Eruptions to Volcanoes", )
+
+            ])
+        ]),
+
+        dbc.Col(width=1)
+    ]),
+])
---
+++ mydash/apps/app4/app4.py
@@ -0,0 +1,71 @@
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from dash import dcc, html, Input, Output
+
+from mydash.app import app
+from mydash.charts_data import VolcanoData
+import mydash.create_charts as CC
+
+data = VolcanoData()
+data.process_data_for_circular_bar()
+
+pp = CC.PolarPlot(data)
+fig1 = pp.create_polar('POP_5')
+
+layout = html.Div(children=[
+    html.Br(),
+    dbc.Row([
+        dbc.Col(width=1),
+
+        dbc.Col(width=10, children=[
+            html.H1(children='Population Near Volcanoes'),
+        ]),
+
+        dbc.Col(width=1)
+    ]),
+
+    html.Br(),
+
+    dbc.Row([
+        dbc.Col(width=1),
+
+        dbc.Col(width=9, children=[
+            dcc.Graph(
+                id='polar-plot',
+                figure=fig1,
+            )
+        ]),
+
+        dbc.Col(width=1)
+    ]),
+
+    dbc.Row([
+        dbc.Col(width=3),
+        dbc.Col(width=6, children=[
+            dcc.Slider(5,
+                       100,
+                       step=None,
+                       marks={5: '5',
+                              10: '10',
+                              30: '30',
+                              100: '100'
+                              },
+                       id='slider'
+                       ),
+            html.P('Distance from the Volcano (km)', style={'textAlign': 'center'})
+        ]),
+        dbc.Col(width=3)
+    ])
+
+])
+
+
+@app.callback(
+    Output('polar-plot', 'figure'),
+    Input('slider', 'value')
+)
+def update_polar(value):
+    fig1 = pp.create_polar('POP_{}'.format(value))
+    return fig1
