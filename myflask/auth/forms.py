c004d80a6055d6d902f9da97e3c9c4f1b05061a3
Khalid Agha <khalid_agha20@hotmail.com>
2022-04-03 01:34:46 +0100

    added formhelpers.html

--- myflask/auth/forms.py
+++ myflask/auth/forms.py
@@ -6,12 +6,12 @@ from myflask.models import User
 
 
 class SignupForm(FlaskForm):
-    first_name = StringField(label='First name', validators=[DataRequired()])
-    last_name = StringField(label='Last name', validators=[DataRequired()])
-    email = EmailField(label='Email address', validators=[DataRequired()])
+    first_name = StringField(label='First Name', validators=[DataRequired()])
+    last_name = StringField(label='Last Name', validators=[DataRequired()])
+    email = EmailField(label='Email Address', validators=[DataRequired()])
     DOB = DateField(label='Date of Birth', validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
-    password_repeat = PasswordField(label='Repeat Password',
+    password_repeat = PasswordField(label='Confirm Pass',
                                     validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     user_type = StringField(label="Account Type", validators=[DataRequired()])
     country = StringField(label='Country', validators=[DataRequired()])
